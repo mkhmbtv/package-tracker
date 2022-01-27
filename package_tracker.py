1c7b27d20e0f439ad2f8cdec74fb4eb40b50e376
your name here <mmakhambetov@yandex.ru>
2022-01-28 02:05:17 +0400

    Add data from the form to database

--- package_tracker.py
+++ package_tracker.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_migrate import Migrate
 from app.config import Config
 from app.shipping_form import ShippingForm
-from app.models import db
+from app.models import db, Package
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,9 +18,16 @@ def index():
 @app.route('/new_package', methods=['GET', 'POST'])
 def add_package():
     form = ShippingForm()
-    if request.method == 'POST':
-        if form.cancel.data:
-            return redirect(url_for('.add_package'))
+    if request.method == 'POST' and form.cancel.data:
+        return redirect(url_for('.add_package'))
     if form.validate_on_submit():
-        return redirect(url_for('.index'))
+        data = form.data
+        new_package = Package(sender=data['sender'],
+                              recipient=data['recipient'],
+                              origin=data['origin'],
+                              destination=data['destination'],
+                              location=data['origin'])
+        db.session.add(new_package)
+        db.session.commit()
+        return redirect('/')
     return render_template('shipping_request.html', form=form)
