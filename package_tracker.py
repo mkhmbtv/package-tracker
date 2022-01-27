b182483c921cd125b820625e497d672c3934462f
your name here <mmakhambetov@yandex.ru>
2022-01-28 01:49:22 +0400

    Create Package model and migration

--- package_tracker.py
+++ package_tracker.py
@@ -1,9 +1,13 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_migrate import Migrate
 from app.config import Config
 from app.shipping_form import ShippingForm
+from app.models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
